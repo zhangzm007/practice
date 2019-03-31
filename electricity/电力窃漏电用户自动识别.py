c73519aa6b7c161602b7ad99c966fa3ab7aa78ab
zhangzm007 <35065764+zhangzm007@users.noreply.github.com>
2019-03-31 11:15:43 +0800

    Update 电力窃漏电用户自动识别.py

--- electricity/电力窃漏电用户自动识别.py
+++ electricity/电力窃漏电用户自动识别.py
@@ -33,7 +33,7 @@ train = model[:int(0.8*len(model)),:]#训练数据
 test = model[int(0.8*len(model)):,:]#测试数据
 m_data = model
 
-#LM神经网络训练模型
+#BP神经网络训练模型
 from keras.models import Sequential
 from keras.layers.core import Dense,Activation
 model = Sequential()
@@ -44,9 +44,32 @@ model.add(Activation('sigmoid'))
 
 model.compile(loss = 'binary_crossentropy', optimizer = 'adam',metrics = ['accuracy'])#指定标准，默认为损失
 model.fit(train[:,:3],train[:,3],batch_size = 10,epochs = 100)
-loss,accuracy = model.evaluate(test[:,:3],test[:,3])#返回为损失值和编译中指定的标准
-model.save_weights(u'D:\\Github\\python_practice\\electricity\\m.model')
+m_loss,m_accuracy = model.evaluate(test[:,:3],test[:,3])#返回为损失值和编译中指定的标准
+model.save_weights(u'D:\\GitHub\\practice\\electricity\\m.model')
 
 from cm_plot import *
-cm_plot(test[:,3],model.predict_classes(test[:,:3]).reshape(len(test))).show()
+cm_plot(test[:,3],model.predict_classes(test[:,:3]).reshape(len(test))).show() 
+#keras里predict_classes得到预测分类结果，predict得到为正结果的概率
+
+#CART决策树
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier()
+tree.fit(train[:,:3],train[:,3])
+tree.predict(test[:,:3])
+cm_plot(test[:,3],tree.predict(test[:,:3]).reshape(len(test))).show()
+
+from sklearn.externals import joblib
+joblib.dump(tree,r'D:\GitHub\practice\electricity\t.pkl')
+#sklearn里predict得到预测结果，predict_proba得到结果为正的概率
+
+#模型评价（ROC曲线）
+from sklearn.metrics import roc_curve
+fpr,tpr,thresholds = roc_curve(test[:,3],model.predict(test[:,:3]).reshape(len(test)),pos_label = 1)
+import matplotlib.pyplot as plt
+plt.plot(fpr,tpr)
+
+t_fpr,t_tpr,t_thresholds = roc_curve(test[:,3],tree.predict_proba(test[:,:3])[:,1],pos_label = 1)
+plt.plot(t_fpr,t_tpr)
+plt.ylim(0,1.05)
+plt.show()
 
